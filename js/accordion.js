c5d3da012096b06054fa116e3b9852da21e998b1
Asep Dendi <142903437+dendiasep@users.noreply.github.com>
2023-09-25 10:32:27 +0700

    Update accordion.js

--- js/accordion.js
+++ js/accordion.js
@@ -6,7 +6,7 @@ for (let i = 0; i < acc.length; i++) {
 acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     const panel = this.nextElementSibling;
-    if (panel.style.display === "flex") {
+    if (panel.style.display === "block") {
     panel.style.display = "none";
     } else {
     panel.style.display ="block";
